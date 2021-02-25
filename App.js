19930125e96b71983624c8a86d671274c516e0db
_ <your_email@whatever.com>
2021-02-25 18:51:35 +0300

    del all

--- App.js
+++ App.js
@@ -4,28 +4,16 @@ import {View, Text} from "react-native"
 class App extends React.Component{ 
   render(){
       return (
-        <View 
-        style={{ 
-          flex:1,
-          justifyContent: "center", 
-          alignItems: "center" 
-        }}
-      
-      >
-      <Text>Hello yoy</Text>
-      <Text>Hello yoy</Text>
-      <Text>Hello yoy</Text>
-      <Text>Hello yoy</Text>
-      </View>
+        <View/> 
       ) 
   }
 }
+export default App;
+
+
+
 
 let age = 21;
 alert(age);
 alert("!тевирП");
 alert("ggggg")
-
-
-
-export default App;
