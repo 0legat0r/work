969d001c5e0cbd19c06c03dd05615497c61b6729
_ <your_email@whatever.com>
2021-02-25 18:17:58 +0300

    my work

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from "react"
+import {View, Text} from "react-native"
+
+class App extends React.Component{ 
+  render(){
+      return (
+        <View 
+        style={{ 
+          flex:1,
+          justifyContent: "center", 
+          alignItems: "center" 
+        }}
+      
+      >
+      
+      <Text>Hello</Text>
+      <Text>Hello yoy</Text>
+      <Text>Hello yoy</Text>
+      <Text>Hello yoy</Text>
+      <Text>Hello yoy</Text>
+      </View>
+      ) 
+  }
+}
+
+let age = 21;
+alert(age);
+alert("!тевирП");
+
+
+
+export default App;
